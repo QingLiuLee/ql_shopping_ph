349efa795e0b8bc3d683a7cffd86020d15eeae6b
liuhuihui <244701374@qq.com>
2019-04-30 16:30:44 +0800

    vant 测试使用

---
+++ client/src/views/Mine.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="mall-mine">
+    <h3>我的</h3>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  components: {
+  },
+})
+export default class Mine extends Vue {}
+</script>

5ec8ef5abf622e2bf76fba660bce2ee06e46c4e9
liuhuihui <244701374@qq.com>
2019-05-05 18:34:43 +0800

    home

--- client/src/components/home/tabcon.vue
+++ client/src/components/home/tabcon.vue
@@ -1,36 +1,92 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <van-button type="default">默认按钮</van-button>
+  <div class="h-tab-all">
+    <!--<h2 @click="sendChildMsg('这是子组件的值')">{{msg}}</h2>-->
+    <van-tabs v-model="active" sticky animated swipeable @click="checkTab">
+      <van-tab :title="item.name" v-for="item in tabList">
+        <div class="h-tab-item-all">
+          <div class="mall-card h-tab-item" v-for="item in data">
+            <img :src="item.img" alt="">
+            <p class="h-tab-item-name">{{item.name}}</p>
+            <p>
+              <span class="h-tab-item-price">￥{{item.price}}</span>
+              <span class="h-tab-item-payment">{{item.payment}}人付款</span>
+              <span class="h-tab-item-more"><van-icon name="ellipsis" /></span>
+            </p>
+          </div>
+        </div>
+      </van-tab>
+    </van-tabs>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
-// import { Button } from 'vant';
+import { Component, Prop, Vue, Emit } from 'vue-property-decorator';
+import { Tab, Tabs } from 'vant';
+Vue.use(Tab).use(Tabs);
 
 @Component({
-  // [Button.name]: Button
 })
 export default class TabCon extends Vue {
-  @Prop() private msg!: string;
+    /*prop*/
+    @Prop() msg:string;
+    @Prop() tabList: any;
+
+    /*data*/
+    active:number = 0;
+    data:any = [
+        {
+            img: require('../../assets/images/sp_01.jpg'),
+            name: '可爱女童动漫卡通T恤',
+            price: '88.88',
+            payment:'1200'
+        },
+        {
+            img: require('../../assets/images/sp_02.jpg'),
+            name: '可爱女童动漫卡通T恤',
+            price: '88.88',
+            payment:'1200'
+        },
+        {
+            img: require('../../assets/images/sp_03.jpg'),
+            name: '可爱女童动漫卡通T恤',
+            price: '88.88',
+            payment:'1200'
+        },
+        {
+            img: require('../../assets/images/sp_04.jpg'),
+            name: '可爱女童动漫卡通T恤',
+            price: '88.88',
+            payment:'1200'
+        },
+        {
+            img: require('../../assets/images/sp_05.jpg'),
+            name: '可爱女童动漫卡通T恤可爱女童动漫卡通T恤',
+            price: '88.88',
+            payment:'1200'
+        },
+        {
+            img: require('../../assets/images/sp_06.jpg'),
+            name: '可爱女童动漫卡通T恤可爱女童动漫卡通T恤可爱女童动漫卡通T恤',
+            price: '88.88',
+            payment:'1200'
+        },
+    ];
+
+    /*methods*/
+    @Emit('sendMsg')
+    sendMsgs(message:string){
+        console.log(message);
+    }
+
+    //通过触发此方法，传值给父组件
+    sendChildMsg(message:string){
+        this.sendMsgs(message);
+    }
+
+    checkTab(index, title) {
+        this.$toast(title);
+    }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+<style scoped lang="scss"></style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,56 +1,166 @@
 <template>
-  <div class="mall-home">
-    <div class="mall-head">
-      <span></span><span>首页</span><span></span>
-    </div>
+    <div class="mall-home">
+        <!--head-->
+        <div class="mall-head">
+            <!--<span></span><span>首页</span><span></span>-->
+            <span><van-icon name="qr" /></span>
+            <van-search
+                    placeholder="请输入搜索关键词"
+                    v-model="value"
+                    shape="round"
+            />
+            <span><van-icon name="chat-o" /></span>
+        </div>
 
-    <div class="h-carousel">
-      <van-swipe :autoplay="3000" indicator-color="white">
-  <van-swipe-item>1</van-swipe-item>
-  <van-swipe-item>2</van-swipe-item>
-  <van-swipe-item>3</van-swipe-item>
-  <van-swipe-item>4</van-swipe-item>
-</van-swipe>
-    </div>
+        <!--轮播图-->
+        <div class="h-carousel">
+            <van-swipe :autoplay="3000" indicator-color="white">
+                <van-swipe-item v-for="item in images">
+                    <img :src="item" alt="">
+                </van-swipe-item>
+            </van-swipe>
+        </div>
 
-    <div class="h-cricle">
-      <div><span style="background: #FF6161;"><i class="icon-f-shop" /></span></div>
-      <div><span style="background: #F7A0A0;"><i class="icon-f-shop" /></span></div>
-      <div><span style="background: #D8BDF2;"><i class="icon-f-shop" /></span></div>
-      <div><span style="background: #BCE2EF;"><i class="icon-f-shop" /></span></div>
-    </div>
+        <!--圆形操作块-->
+        <div class="h-cricle">
+            <div><span style="background: #FF6161;"><i class="icon-f-shop"/></span></div>
+            <div><span style="background: #F7A0A0;"><i class="icon-f-shop"/></span></div>
+            <div><span style="background: #D8BDF2;"><i class="icon-f-shop"/></span></div>
+            <div><span style="background: #BCE2EF;"><i class="icon-f-shop"/></span></div>
+        </div>
 
-    <div class="h-tab">
-      <TabCon />
+        <!--今日特价，9块9 包邮-->
+        <div class="mall-card h-tj">
+            <div class="h-tj-item">
+                <img :src="tj_url" alt="">
+                <div class="h-tj-item-title">
+                    <h4>天天特价</h4>
+                    <p>全网最低 + 超实惠</p>
+                </div>
+                <van-tag round color="#FF6161" text-color="#fff">立即购买</van-tag>
+            </div>
+            <div class="h-tj-item">
+                <img :src="tj_url2" alt="">
+                <div class="h-tj-item-title h-tj-item-title2">
+                    <h4>天天特价</h4>
+                    <p>全网最低 + 超实惠</p>
+                </div>
+                <van-tag round color="#FF6161" text-color="#fff">立即购买</van-tag>
+            </div>
+        </div>
+
+        <!--活动-->
+        <div class="mall-card h-hd">
+            <span class="h-hd-title">活动</span>
+            <van-notice-bar>
+                <van-tag color="#FFECEC" text-color="#FF6161">店铺周年庆</van-tag>
+                diro香水周年庆，199带回家！！！
+            </van-notice-bar>
+        </div>
+
+        <!--产品促销，居家必备...-->
+        <div class="mall-card h-cx">
+            <div class="h-cx-l">
+                <span class="h-cx-title">今日促销</span>
+                <span style="margin-left: 10px">
+                    <van-tag color="#FF6161" text-color="#fff">08</van-tag>
+                    <span style="margin: 0 2px;font-weight: bold">:</span>
+                    <van-tag color="#FF6161" text-color="#fff">11</van-tag>
+                    <span style="margin: 0 2px;font-weight: bold">:</span>
+                    <van-tag color="#FF6161" text-color="#fff">21</van-tag>
+                </span>
+                <div>
+                    <img :src="hd_images[0]" alt="">
+                    <img :src="hd_images[1]" alt="">
+                    <img :src="hd_images[2]" alt="">
+                    <img :src="hd_images[4]" alt="">
+                </div>
+            </div>
+
+            <div class="h-cx-r">
+                <div class="h-cx-r-item">
+                    <span class="h-cx-title">居家必备</span>
+                    <div>
+                        <img :src="hd_images[4]" alt="">
+                        <img :src="hd_images[5]" alt="">
+                    </div>
+                </div>
+                <div class="h-cx-r-item">
+                    <span class="h-cx-title">节日好礼</span>
+                    <div>
+                        <img :src="hd_images[3]" alt="">
+                        <img :src="hd_images[5]" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!--商品-->
+        <div class="h-tab">
+            <TabCon
+                    :tabList="tabList"
+                    :msg="msg"
+                    @sendMsg="getChildMsg"
+            />
+        </div>
     </div>
-  </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-import TabCon from "@/components/home/tabcon.vue"; // @ is an alias to /src
-import { Swipe, SwipeItem } from "vant";
-Vue.use(Swipe).use(SwipeItem);
+    import {Component, Vue} from "vue-property-decorator";
+    import TabCon from "@/components/home/tabcon.vue";
+    import {Swipe, SwipeItem, NoticeBar, Search} from "vant";
 
-@Component({
-  components: {
-    TabCon
-  }
-})
-export default class Home extends Vue {
-  private images: any[] = [
-    "https://img.yzcdn.cn/1.jpg",
-    "https://img.yzcdn.cn/2.jpg"
-  ];
-}
+    Vue.use(Swipe).use(SwipeItem).use(NoticeBar).use(Search);
+
+    @Component({
+        components: {
+            TabCon
+        }
+    })
+    export default class Home extends Vue {
+        /*data*/
+        images: any[] = [
+            require('../assets/images/banner_01.jpg'),
+            require('../assets/images/banner_02.png'),
+            require('../assets/images/banner_03.jpg')
+        ];
+        hd_images: any[] = [
+            require('../assets/images/sp_01.jpg'),
+            require('../assets/images/sp_02.jpg'),
+            require('../assets/images/sp_03.jpg'),
+            require('../assets/images/sp_04.jpg'),
+            require('../assets/images/sp_05.jpg'),
+            require('../assets/images/sp_06.jpg')
+        ];
+        msg: string = 'hello';
+        message: string = 'message';
+        tabList:any = [
+            { name: '全部'},
+            { name: '女装上衣'},
+            { name: '美妆'},
+            { name: '酒水'},
+            { name: '娱乐'},
+            { name: '家具'},
+        ];
+
+        private tj_url = require('../assets/images/sp_01.jpg');
+        private tj_url2 = require('../assets/images/sp_03.jpg');
+
+        /*methods*/
+        getChildMsg(message:string){
+            this.message = message;
+        }
+    }
 </script>
 
 
 <style lang="scss">
-.van-swipe-item:nth-child(odd) {
-  background-color: #66c6f2;
-}
-.van-swipe-item:nth-child(even) {
-  background-color: #39a9ed;
-}
+    .van-swipe-item:nth-child(odd) {
+        background-color: #66c6f2;
+    }
+
+    .van-swipe-item:nth-child(even) {
+        background-color: #39a9ed;
+    }
 </style>
